addfb7db14d428c21b0d56b6426722df26ca3013
Firore <takbasic722@gmail.com>
2018-06-13 00:23:53 +0700

    อัพวันที่ 1

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<?php include 'inc_js.php';
+              include 'form/main_form.php';
+              include 'form/gridview.php';
+		?>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/jquery-ui.css">
+		<link rel="stylesheet" href="CSS/scss/_input-group.scss">
+        <link rel="stylesheet" href="CSS/main.css">
+		<title></title>
+	</head>
+	<body>
+		<div class="warpper">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="row">
+							<nav class="navbar navbar-light col-lg-12 bn-header">
+								<div class="col-lg-2">
+									<a class="navbar-brand" href="#">
+										<img src="images/noimage.png" width="30" height="30" class="d-inline-block align-top" alt="">
+										No Logo
+									</a>
+								</div>
+								<div class="col-lg-10">
+									<p class="bn-menuhead">Menu Name</p>
+									<ul class="nav navbar-nav float-right">
+										<li class="nav-item dropdown">
+												<img src="images/noimage.png" width="30" height="30" class="nav-link dropdown-toggle d-inline-block align-top bn-profile" style="border-radius:50%;" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+											<div class="dropdown-menu">
+												<a class="dropdown-item" href="#">Action</a>
+												<a class="dropdown-item" href="#">Another action</a>
+												<a class="dropdown-item" href="#">Something else here</a>
+												<div class="dropdown-divider"></div>
+											</div>
+										</li>
+									</ul>
+									<ul class="nav navbar-nav float-right">
+										<li class="nav-item dropdown">
+												<img src="images/noimage.png" width="30" height="30" class="nav-link dropdown-toggle d-inline-block align-top bn-submenu" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+											<div class="dropdown-menu">
+												<a class="dropdown-item" href="#">Action</a>
+												<a class="dropdown-item" href="#">Another action</a>
+												<a class="dropdown-item" href="#">Something else here</a>
+												<div class="dropdown-divider"></div>
+											</div>
+										</li>
+									</ul>
+									<ul class="nav navbar-nav float-right">
+										<li class="nav-item dropdown">
+												<img src="images/noimage.png" width="30" height="30" class="nav-link dropdown-toggle d-inline-block align-top bn-submenu" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+											<div class="dropdown-menu">
+												<a class="dropdown-item" href="#">Action</a>
+												<a class="dropdown-item" href="#">Another action</a>
+												<a class="dropdown-item" href="#">Something else here</a>
+												<div class="dropdown-divider"></div>
+											</div>
+										</li>
+									</ul>
+								</div>
+							</nav>
+						</div>
+					</div>
+					<div class="col-12">
+						<div class="row">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
